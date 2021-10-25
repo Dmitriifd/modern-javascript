beddbb7a49e90bc74b9f0ce4938b8d60528fede6
mitriifd <idimka9@gmail.com>
2021-10-25 19:42:33 +0500

    async upd

---
+++ Асинхронный JavaScript/Promises/app.js
@@ -0,0 +1,20 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(Math.random())
+    }, 1000);
+})
+
+
+console.log(promise);
+
+promise
+    .then(x => {
+        console.log(x);
+        return x;
+    })
+    .then(y => {
+        console.log(y);
+    })
+    .catch(err => console.log(err));
+
+promise.then(z => console.log(z));
